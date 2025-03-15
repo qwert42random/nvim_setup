19d99163c77bcd76aec10fa8358f76eba0ec3cf2
Christopher Chung <Cskywalker1@gmail.com>
2025-03-15 18:26:29 +0000

    Feat: clone neovim repo to /tmp dir

--- install/install_script.sh
+++ install/install_script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+NVIM_REPO=/tmp/neovim
+
 set -e
 
 # Install dependencies.
@@ -8,8 +10,10 @@ for DEB in "${DEP_LIST[@]}"; do
     apt-get install ${DEB} -y
 done
 
-git clone https://github.com/neovim/neovim.git --depth 1 --branch "stable"
+git clone https://github.com/neovim/neovim.git --depth 1 --branch "stable" ${NVIM_REPO}
 
-make -C neovim CMAKE_BUILD_TYPE=RelWithDebInfo
-make -C neovim install
+make -C ${NVIM_REPO} CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=/usr
+make -C ${NVIM_REPO} install
+
+mkdir -p ~/.config/nvim
 
