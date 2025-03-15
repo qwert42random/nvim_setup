c50e3e351951d1601f902a797708646f2f82bf5f
Christopher Chung <Cskywalker1@gmail.com>
2025-03-15 18:26:29 +0000

    Feat: add HOME_DIR var to install script
    Clean dependencies install lines.

--- install/install_script.sh
+++ install/install_script.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 NVIM_REPO=/tmp/neovim
+HOME_DIR=(eval echo ~${SUDO_USER})
 
 set -e
 
 # Install dependencies.
-DEP_LIST=( "git" "make" "cmake" "gettext" )
-for DEB in "${DEP_LIST[@]}"; do
-    apt-get install ${DEB} -y
-done
+DEPENDENCIES="git make cmake gettext"
+apt-get install -y ${DEPENDENCIES}
 
 git clone https://github.com/neovim/neovim.git --depth 1 --branch "stable" ${NVIM_REPO}
 
 make -C ${NVIM_REPO} CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=/usr
 make -C ${NVIM_REPO} install
 
-mkdir -p ~/.config/nvim
+mkdir -p $HOME_DIR/.config/nvim
 
