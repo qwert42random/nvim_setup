63693db8ceca9e70d6edb2ab95e88f27bcaef140
Christopher Chung <Cskywalker1@gmail.com>
2025-03-15 18:26:29 +0000

    File: add init.lua file

---
+++ init.lua
@@ -0,0 +1,13 @@
+vim.opt.number = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.cursorline = true
+
+-- vim.opt.syntax = true
+-- vim.opt.backspace = true
+-- set autoindent
+-- set ruler
+-- set cursorline
+-- set showcmd
+
