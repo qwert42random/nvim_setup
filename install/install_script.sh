13518e6dd2a1f37774004fd5efff6ea067a3f337
Christopher Chung <Cskywalker1@gmail.com>
2025-03-15 18:26:29 +0000

    Feat: move init.lua
    Also add init.lua to install script.

--- install/install_script.sh
+++ install/install_script.sh
@@ -3,17 +3,24 @@
 NVIM_REPO=/tmp/neovim
 HOME_DIR=$(eval echo ~${SUDO_USER})
 
+CONFIG_LOC=$HOME_DIR/.config/nvim
+PLUGIN_LOC=$HOME_DIR/.local/share/nvim/site/pack/plugins
+
 set -e
 
 # Install dependencies.
 DEPENDENCIES="git cmake gettext"
-apt-get install -y ${DEPENDENCIES}
+sudo apt-get install -y ${DEPENDENCIES}
 
 git clone https://github.com/neovim/neovim.git --depth 1 --branch "stable" ${NVIM_REPO}
 
 make -C ${NVIM_REPO} CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=/usr
-make -C ${NVIM_REPO} install
+sudo make -C ${NVIM_REPO} install
 
-mkdir -p $HOME_DIR/.config/nvim
-mkdir -p $HOME_DIR/.local/share/nvim/site/pack/plugins/start
+mkdir -p $CONFIG_LOC
+mkdir -p $PLUGIN_LOC/start
+
+cp init.lua $CONFIG_LOC
+
+# Install plugins
 
