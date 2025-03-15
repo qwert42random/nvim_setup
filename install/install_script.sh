f1847d1e01e855bb06dd142ffb07617677ab8752
Christopher Chung <Cskywalker1@gmail.com>
2025-03-15 18:26:29 +0000

    Feat: add plugin installation
    Install script now checks if neovim installed.
    Update sudo usage.

--- install/install_script.sh
+++ install/install_script.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 
+set -eu
+
+SCRIPT_DIR=$(dirname $BASH_SOURCE)
+
 NVIM_REPO=/tmp/neovim
-HOME_DIR=$(eval echo ~${SUDO_USER})
 
-CONFIG_LOC=$HOME_DIR/.config/nvim
-PLUGIN_LOC=$HOME_DIR/.local/share/nvim/site/pack/plugins
-
-set -e
+CONFIG_LOC=~/.config/nvim
+PLUGIN_LOC=~/.local/share/nvim/site/pack/plugins
 
 # Install dependencies.
 DEPENDENCIES="git cmake gettext"
 sudo apt-get install -y ${DEPENDENCIES}
 
-git clone https://github.com/neovim/neovim.git --depth 1 --branch "stable" ${NVIM_REPO}
-
-make -C ${NVIM_REPO} CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=/usr
-sudo make -C ${NVIM_REPO} install
+# TODO: Check if version is correct?
+# Install neovim if it does not exist.
+if [ ! -x "$(command -v nvim)" ]; then
+    git clone https://github.com/neovim/neovim.git --depth 1 --branch "stable" ${NVIM_REPO}
+    make -C ${NVIM_REPO} CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=/usr
+    sudo make -C ${NVIM_REPO} install
+fi
 
 mkdir -p $CONFIG_LOC
 mkdir -p $PLUGIN_LOC/start
 
-cp init.lua $CONFIG_LOC
+cp $SCRIPT_DIR/init.lua $CONFIG_LOC
+
+PLUGINS=(
+"https://github.com/nvim-lualine/lualine.nvim.git --branch 'compat-nvim-0.6'" \
+"https://github.com/nvim-tree/nvim-tree.lua.git --branch 'v1.11.0'"
+)
 
 # Install plugins
+for PLUGIN in "${PLUGINS[@]}"; do
+    REPO_NAME=$(basename "$PLUGIN" .git)
+    eval git clone "${PLUGIN}" --depth 1 "${PLUGIN_LOC}/start/${REPO_NAME}"
+done
 
