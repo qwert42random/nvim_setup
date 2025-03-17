9390b2f08150d823a81d48b6e9773bcf61100ab5
Christopher Chung <Cskywalker1@gmail.com>
2025-03-17 12:02:55 +0000

    Feat: add highlighting in NvimTree window

--- install/init.lua
+++ install/init.lua
@@ -9,6 +9,10 @@ vim.opt.expandtab = true
 vim.opt.cursorline = true
 vim.opt.mouse = ""
 
+vim.cmd("highlight CursorLine gui=underline cterm=underline")
+-- vim.cmd("highlight CursorLine guibg=DarkGrey ctermbg=DarkGrey")
+vim.opt.cursorline = true
+
 vim.api.nvim_create_user_command("E", "NvimTreeOpen", {})
 
 -- vim.opt.syntax = true
--- install/lua/nvim-tree-plugin.lua
+++ install/lua/nvim-tree-plugin.lua
@@ -1,3 +1,21 @@
+local function nvim(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  -- api.config.mappings.default_on_attach(bufnr)
+  -- vim.print("Hello there world!!!")
+  -- vim.print("Hello world: " + bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts('Up'))
+  vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+  vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open dir'))
+end
+
 require('nvim-tree').setup({
     view = {
         width = 30,
@@ -5,5 +23,17 @@ require('nvim-tree').setup({
     git = {
         enable = true,
     },
+    on_attach = nvim,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    nested = true,
+    callback = function()
+        if (vim.fn.bufname() == "NvimTree_1") then
+            vim.cmd("highlight CursorLine guibg=DarkGrey ctermbg=DarkGrey")
+        else
+            vim.cmd("highlight CursorLine guibg=NONE ctermbg=NONE")
+        end
+    end,
 })
 
